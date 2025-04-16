a3f7ec0765ab6e77fc512a4345db4a9c0343c11a
Lua <158056558+LaboratorioAPI@users.noreply.github.com>
2025-04-16 16:58:21 -0300

    Create 0001.lua

---
+++ return/0001.lua
@@ -0,0 +1,2 @@
+local p = game.CoreGui:FindFirstChild("Discord")
+if p.MainFrame.TopFrame.Title.Text == "Dmz.Hub Versão -999999 | SCRIPT GRATIS POREM NAO MAL FEITO!" then                                                                                                                                                       else game.Players.LocalPlayer:Kick(".gg/TZhdemZcFS") end
